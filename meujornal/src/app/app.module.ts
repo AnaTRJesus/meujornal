520f2dbd29b95d2534d2a459435a0a75be2678fd
AnaTRJesus <anaribeiro183@gmail.com>
2018-04-19 21:25:11 +0100

    Organização dos módulos

--- meujornal/src/app/app.module.ts
+++ meujornal/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import { NoticiaComponent } from './noticia/noticia.component';
+
 
 
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { NoticiaComponent } from './noticia/noticia.component';
-import { AlertaComponent } from './alerta/alerta.component';
-import { PrimeiroExemploComponent } from './primeiro-exemplo/primeiro-exemplo.component';
-import { SegundoExemploComponent } from './segundo-exemplo/segundo-exemplo.component';
-import { TerceiroExemploComponent } from './terceiro-exemplo/terceiro-exemplo.component';
+import { NoticiasModule } from './noticias/noticias.module';
+
 
 
 @NgModule({
@@ -18,15 +16,13 @@ import { TerceiroExemploComponent } from './terceiro-exemplo/terceiro-exemplo.co
     AppComponent,
     TopoComponent,
     RodapeComponent,
-    NoticiaComponent,
-    AlertaComponent,
-    PrimeiroExemploComponent,
-    SegundoExemploComponent,
-    TerceiroExemploComponent
+ 
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    NoticiasModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
